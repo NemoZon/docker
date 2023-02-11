d70775ddf2b4a880640e1b1c63047d1f04baed47
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2023-02-11 22:18:54 +0100

    Docker compose

---
+++ docker-compose-app/app/main.py
@@ -0,0 +1,11 @@
+# Приложение выводит список баз данных из mongodb
+from pymongo import MongoClient # Этот модуль не скачен на моем компьютере
+from pprint import pprint
+
+MONGO_URL = "mongodb://mongo:27017" # Адрес другого сервиса с использование его имени указаного в composer и стандартным портом для базы данных
+client = MongoClient(MONGO_URL)
+db = client.admin
+dbs_list = db.command("listDatabases")
+pprint(dbs_list)
+
+print("Exit")
